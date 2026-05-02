27b21a8ab143d792ffafd453e77bc8396b7cc422
EC2 Default User <ec2-user@ip-172-31-24-225.eu-west-2.compute.internal>
2026-05-02 07:29:59 +0000

    워크 플로어 등록

--- springboot-app/hello/src/main/java/com/example/GreetingController.java
+++ springboot-app/hello/src/main/java/com/example/GreetingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s ^__^";
+	private static final String template = "Hello, %s ^___^";
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/hello")
